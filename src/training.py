5a9f6a8aecc4f33d21d0d109057ad4057c2ed753
Brandon Qiu <brandonqiu777@gmail.com>
2024-12-04 00:28:37 -0500

    changed training based on weather api

--- src/training.py
+++ src/training.py
@@ -1,28 +1,26 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error
 
 def train_model(datapath):
     data = pd.read_csv(datapath)
-
-    X = data[['Clearsky GHI', 'Clearsky DHI',  
-            'Solar Zenith Angle', 'Cloud Type', 
-            'Relative Humidity', 'Temperature']]
     
-    y = data['GHI']
+    X = data[['Temperature', 'Relative Humidity', 'Cloud Type', 'Solar Zenith Angle', 'Wind Speed', 'Pressure']]
+    
+    y = data['GHI']  # Global Horizontal Irradiance (what we want to predict)
 
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     model = RandomForestRegressor(
-    n_estimators=200,
-    max_depth=10,
-    min_samples_split=5,
-    min_samples_leaf=3,
-    max_features='sqrt',
-    random_state=42
-)
+        n_estimators=200,
+        max_depth=10,
+        min_samples_split=5,
+        min_samples_leaf=3,
+        max_features='sqrt',
+        random_state=42
+    )
+
     model.fit(X_train, y_train)
 
     predictions = model.predict(X_test)
@@ -30,9 +28,7 @@ def train_model(datapath):
 
     return model, mae
 
-def save_model(model, path="models/trained.pkl"):
+def save_model(model, path="model/trained.pkl"):
     import joblib
-    joblib.dump(model,path)
+    joblib.dump(model, path)
 
-
-    
